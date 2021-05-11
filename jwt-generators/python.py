3b5dffcaa33483ca296cc857eb3124864d874d90
Swaathi Kakarla <swaathi@users.noreply.github.com>
2021-05-11 20:26:08 +0530

    Update python.py

--- jwt-generators/python.py
+++ jwt-generators/python.py
@@ -7,7 +7,9 @@ SSO_KEY = "GENERATED_SSO_KEY";
 def generate_sso_token(user):
   user_data = {
     'email': user.email,
-    'name': user.name,
+    'name': user.name
+    # if you'd like to add the person to your team, pass this option
+#     'add_to_team': True
   }
   
   return jwt.encode(user_data, SSO_KEY, algorithm='HS256')
