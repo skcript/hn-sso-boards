b877b276bcb19cdda51f0f1f867b653cdec18927
Swaathi Kakarla <swaathi@users.noreply.github.com>
2021-05-11 20:25:46 +0530

    Update ruby.rb

--- jwt-generators/ruby.rb
+++ jwt-generators/ruby.rb
@@ -8,6 +8,8 @@ def generate_sso_token(email, name)
   user_data = {
     email: email,
     name: name
+# if you'd like to add the person to your team, pass this option
+#     add_to_team: true
   }
 
   JWT.encode(user_data, SSO_KEY, 'HS256')
