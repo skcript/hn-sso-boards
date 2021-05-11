9e9079a89db780f158db9db1e0bbcba3bfe78f02
Swaathi Kakarla <swaathi@users.noreply.github.com>
2021-05-11 20:27:31 +0530

    Update javascript.js

--- jwt-generators/javascript.js
+++ jwt-generators/javascript.js
@@ -12,6 +12,8 @@ function generateJWTToken(user) {
   var userData = {
     email: user.email,
     name: user.name,
+    # if you'd like to add the person to your team, pass this option
+    # add_to_team: true
   };
   return jwt.sign(userData, SSO_KEY, { algorithm: "HS256" });
 }
